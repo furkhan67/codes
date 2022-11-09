bd761ed2825deb31d4963545e780123f5c5f49a4
furkhan67 <mohammedfurkhan67@gmail.com>
2022-11-09 16:13:02 +0000

    rmttsw

--- projects/python/speech/ttsw.py
+++
@@ -1,4 +0,0 @@
-from tts_watson.TtsWatson import TtsWatson
-
-ttsWatson = TtsWatson('mohammedfurkhan67@gmail.com', 'Nnnoman@007', 'en-US_AllisonVoice') # en-US_AllisonVoice is a voice from watson you can found more to: https://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/doc/text-to-speech/using.shtml#voices
-ttsWatson.play("The text which i want to be a sound")
